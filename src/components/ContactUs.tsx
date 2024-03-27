daa021e253b367e9a22bd900f6a3c83b221d018d
Lucio D'Alessandro <lda@juxt.pro>
2024-03-28 00:03:02 +0100

    Update form color

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -16,7 +16,7 @@ export function ContactUs() {
             <input
               type="email"
               id="email"
-              className="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+              className="focus:ring-primary-500 focus:border-primary-500   block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm"
               placeholder="esempio@email.com"
               required
             />
@@ -29,7 +29,7 @@ export function ContactUs() {
             <textarea
               id="message"
               rows={6}
-              className="focus:ring-primary-500 focus:border-primary-500 dark:focus:ring-primary-500 dark:focus:border-primary-500 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+              className="focus:ring-primary-500 focus:border-primary-500  block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 shadow-sm"
               placeholder="Messaggio..."
             ></textarea>
           </div>
