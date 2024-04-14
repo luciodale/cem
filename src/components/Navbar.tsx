4aa9e421a5b653960dfe4f2dad0e8bcc9ea92732
Lucio D'Alessandro <lda@juxt.pro>
2024-04-14 03:01:31 +0200

    Fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,20 +14,25 @@ export default function Navbar({ currentHref }: { currentHref: string }) {
       <div className="mx-auto flex h-full max-w-screen-xl items-center justify-between px-4 py-1">
         <a
           href="/"
-          className="flex w-1/6 items-center justify-center space-x-3 rtl:space-x-reverse"
+          className="flex items-center justify-center space-x-3 sm:w-1/6 rtl:space-x-reverse"
         >
           <img src="/logo.svg" className="w-28" alt="Cem Logo" />
         </a>
         <button
           id="burger-button"
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          onClick={() => {
+            if (isMenuOpen) document.body.style.overflow = "auto";
+            else document.body.style.overflow = "hidden";
+
+            setIsMenuOpen(!isMenuOpen);
+          }}
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
+          className="inline-flex h-14 w-14 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 lg:hidden"
         >
           <span className="sr-only">Open main menu</span>
           <svg
-            className="h-5 w-5"
+            className="h-6 w-6"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 17 14"
@@ -47,7 +52,7 @@ export default function Navbar({ currentHref }: { currentHref: string }) {
             href={href}
             className={classNames(
               { "text-yellow-500": currentHref === href },
-              "hidden w-1/6 rounded py-2 text-center  text-xl uppercase text-gray-900 hover:bg-gray-100 md:block md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-gray-700",
+              "hidden w-1/6 rounded py-2 text-center text-xl uppercase text-gray-900 hover:bg-gray-100 lg:block lg:border-0 lg:p-0 lg:hover:bg-transparent lg:hover:text-gray-700",
             )}
           >
             {name}
